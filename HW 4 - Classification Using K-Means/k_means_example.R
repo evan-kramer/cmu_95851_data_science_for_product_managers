1bf6d7dfc6a887f29ae2cb8420e9991d44f7b2e9
Evan Kramer <kramer.evan@gmail.com>
2020-10-08 22:53:53 -0400

    Finalized HW 3 and started on HW 4 (including replicating in R)

---
+++ HW 4 - Classification Using K-Means/k_means_example.R
@@ -0,0 +1,18 @@
+# https://www.business-science.io/business/2016/08/07/CustomerSegmentationPt1.html
+library(readxl); library(tidyverse)
+# Prep data
+data = full_join(read_excel('orders.xlsx'),
+                 read_excel('bikes.xlsx'),
+                 by = c('product.id' = 'bike.id')) %>% 
+  full_join(read_excel('bikeshops.xlsx'), by = c('customer.id' = 'bikeshop.id')) %>% 
+  group_by(bikeshop.name, model, category1, category2, frame, price) %>% 
+  summarize(total.qty = sum(quantity, na.rm = T)) %>% 
+  spread(bikeshop.name, total.qty) %>%
+  ungroup() %>%
+  mutate(across(.cols = `Albuquerque Cycles`:`Wichita Speed`, 
+                .fns = ~if_else(is.na(.x), 0, .x / sum(.x, na.rm = T))),
+         price.cat = if_else(price >= median(price, na.rm = T), 'high', 'low'))
+
+# K-means clustering
+
+print(data)
